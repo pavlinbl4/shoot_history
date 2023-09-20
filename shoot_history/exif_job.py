70f69d4bb6a5f65505f7575df19691199452eed2
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-09-20 10:17:00 +0300

    try to write function to back up metadata from image

--- shoot_history/exif_job.py
+++ shoot_history/exif_job.py
@@ -1,4 +1,7 @@
 import exiftool
+from colorama import Fore, Back, Style
+import re
+
 
 
 def change_color_class(file, image_title, color):
@@ -26,3 +29,70 @@ def write_data_to_photo_iptc_tools(file):
                   'XMP:Creator': 'Eugene Pavlenko/exiftool'},
             params=["-P", "-overwrite_original"]
         )
+
+def read_image_metadate(path_to_file):
+
+    with exiftool.ExifToolHelper() as et:
+        metadata = et.get_metadata(path_to_file)
+        tags = et.get_tags(path_to_file, tags=['XMP:*'])[0]
+        tags = et.get_tags([path_to_file], tags=['XMP:Description',
+                                                 'XMP:City',
+                                                 'XMP:DateCreated',
+                                                 'XMP:Urgency',
+                                                 'XMP:ColorMode',
+                                                 'XMP:CreateDate',
+                                                 'XMP:Label',
+                                                 'XMP:Rating',
+                                                 'XMP:PreservedFileName',
+                                                 'XMP:Creator',
+                                                 'XMP:Rights',
+                                                 'XMP:Subject',
+                                                 'XMP:Country',
+                                                 'XMP:CountryCode',
+                                                 'XMP:Title']
+                           )[0]
+        # test = et.encoding  # str UTF-8
+        # test = et.check_tag_names   # <class 'bool'> True
+        # test = et.auto_start   # <class 'bool'> True
+
+        # return metadata[0]
+    return tags
+
+
+
+if __name__ == '__main__':
+    # m_date = read_image_metadate('/Users/evgeniy/Pictures/2023/20230821_недвижимость/20230821EPAV3388.ORF')
+    m_date = read_image_metadate('/Users/evgeniy/Downloads/KSP_017764_00642_1h.jpg')
+    print(type(m_date))
+    print(m_date)
+    # pattern = r'(?<=RU ).+(?= Фото:)'
+    # print(re.findall(pattern, m_date))
+    # for i in m_date.keys():
+    #     if i.startswith('EXIF'):
+    #         print(i)
+    # print(f"{Fore.RED}{m_date.get('XMP:Subject') } ")
+
+    # print(f"{Fore.RED}{m_date.get('IPTC:Caption-Abstract')} ")
+    # print(f"{Fore.RED}{m_date.get('EXIF:ImageDescription') } ")
+    # print(f"{Fore.RED}{m_date.get('XMP:Description') } ")
+    # print()
+    # print(f"{Fore.RED}{m_date.get('IPTC:CopyrightNotice')} ")
+    # print(f"{Fore.RED}{m_date.get('XMP:Rights') } ")
+    # print()
+    # print(f"{Fore.RED}{m_date.get('XMP:Credit') } ")
+    # print(f"{Fore.RED}{m_date.get('IPTC:Credit')} ")
+    # print()
+    # print(f"{Fore.RED}{m_date.get('Photoshop:SlicesGroupName') } ")
+    # # print(f"{Fore.RED}{m_date.get('IPTC:ExifCameraInfo') } ")
+    # print(f"{Fore.RED}{m_date.get('IPTC:ObjectName') } ")
+
+
+
+    # for k,v in m_date.items():
+    #     print(k,v)
+
+
+
+
+
+
