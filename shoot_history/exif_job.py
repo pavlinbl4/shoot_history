24fed51f8196a6ad9e5861e2f6abd5e48842023e
evgeniy <pavlinbl4@gmail.com>
2023-09-21 10:42:28 +0300

    create function to cleear all kp info from image

--- shoot_history/exif_job.py
+++ shoot_history/exif_job.py
@@ -1,7 +1,6 @@
 import exiftool
-from colorama import Fore, Back, Style
-import re
 
+from kp_selenium_tools.regex_tools import modify_caption
 
 
 def change_color_class(file, image_title, color):
@@ -30,69 +29,48 @@ def write_data_to_photo_iptc_tools(file):
             params=["-P", "-overwrite_original"]
         )
 
-def read_image_metadate(path_to_file):
 
+def write_edited_tags(xmp_tags):
     with exiftool.ExifToolHelper() as et:
-        metadata = et.get_metadata(path_to_file)
-        tags = et.get_tags(path_to_file, tags=['XMP:*'])[0]
-        tags = et.get_tags([path_to_file], tags=['XMP:Description',
-                                                 'XMP:City',
-                                                 'XMP:DateCreated',
-                                                 'XMP:Urgency',
-                                                 'XMP:ColorMode',
-                                                 'XMP:CreateDate',
-                                                 'XMP:Label',
-                                                 'XMP:Rating',
-                                                 'XMP:PreservedFileName',
-                                                 'XMP:Creator',
-                                                 'XMP:Rights',
-                                                 'XMP:Subject',
-                                                 'XMP:Country',
-                                                 'XMP:CountryCode',
-                                                 'XMP:Title']
-                           )[0]
-        # test = et.encoding  # str UTF-8
-        # test = et.check_tag_names   # <class 'bool'> True
-        # test = et.auto_start   # <class 'bool'> True
+        et.set_tags({xmp_tags['SourceFile']},
+                    tags={'XMP:Rights': 'Pavlenko Evgeniy',
+                          'XMP:Credit': 'Pavlenko Evgeniy',
+                          'XMP:Creator': 'Pavlenko Evgeniy',
+                          'XMP:Title': '',
+                          'XMP:Label': "Green",
+                          'XMP:Description': xmp_tags['XMP:Description'],
+                          'IPTC:By-line': 'Pavlenko Evgeniy',
+                          'IPTC:Credit': '',
+                          'IPTC:CopyrightNotice': 'Pavlenko Evgeniy',
+                          'IPTC:Caption-Abstract': xmp_tags['XMP:Description'],
+                          'IPTC:ObjectName': '',
+                          'Photoshop:SlicesGroupName': ''
+                          },
+                    params=["-P", "-overwrite_original"])
 
-        # return metadata[0]
-    return tags
 
+def read_image_metadate(path_to_image_file):
+    with exiftool.ExifToolHelper() as et:
+        return et.get_metadata(path_to_image_file)[0]
+
+
+def clear_image_metadata_from_kp_info(path_to_image_file):
+    # read metadata
+    image_metadata = read_image_metadate(path_to_image_file)
+
+    # modify caption
+    modify_caption(image_metadata)
+
+    # save updates metadata to file
+    write_edited_tags(image_metadata)
 
 
 if __name__ == '__main__':
-    # m_date = read_image_metadate('/Users/evgeniy/Pictures/2023/20230821_недвижимость/20230821EPAV3388.ORF')
-    m_date = read_image_metadate('/Users/evgeniy/Downloads/KSP_017764_00642_1h.jpg')
-    print(type(m_date))
-    print(m_date)
-    # pattern = r'(?<=RU ).+(?= Фото:)'
-    # print(re.findall(pattern, m_date))
-    # for i in m_date.keys():
-    #     if i.startswith('EXIF'):
-    #         print(i)
-    # print(f"{Fore.RED}{m_date.get('XMP:Subject') } ")
+    clear_image_metadata_from_kp_info(
+        '/Volumes/big4photo-4/EDITED_JPEG_ARCHIV/Downloaded_from_fotoagency/KSP_017775/KSP_017775_00025_1h.jpg')
 
-    # print(f"{Fore.RED}{m_date.get('IPTC:Caption-Abstract')} ")
-    # print(f"{Fore.RED}{m_date.get('EXIF:ImageDescription') } ")
-    # print(f"{Fore.RED}{m_date.get('XMP:Description') } ")
-    # print()
-    # print(f"{Fore.RED}{m_date.get('IPTC:CopyrightNotice')} ")
-    # print(f"{Fore.RED}{m_date.get('XMP:Rights') } ")
-    # print()
-    # print(f"{Fore.RED}{m_date.get('XMP:Credit') } ")
-    # print(f"{Fore.RED}{m_date.get('IPTC:Credit')} ")
-    # print()
-    # print(f"{Fore.RED}{m_date.get('Photoshop:SlicesGroupName') } ")
-    # # print(f"{Fore.RED}{m_date.get('IPTC:ExifCameraInfo') } ")
-    # print(f"{Fore.RED}{m_date.get('IPTC:ObjectName') } ")
+    m_date = read_image_metadate(
+        '/Volumes/big4photo-4/EDITED_JPEG_ARCHIV/Downloaded_from_fotoagency/KSP_017775/KSP_017775_00025_1h.jpg')
 
-
-
-    # for k,v in m_date.items():
-    #     print(k,v)
-
-
-
-
-
-
+    for k, v in m_date.items():
+        print(k, v)
