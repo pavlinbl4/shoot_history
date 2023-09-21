124b2577e034ffb7556436559e34351c499c9b26
evgeniy <pavlinbl4@gmail.com>
2023-09-21 10:38:32 +0300

    selected regex action in separate function

--- shoot_history/find_file_hdd.py
+++ shoot_history/find_file_hdd.py
@@ -1,14 +1,27 @@
 import os
-import re
+
+from kp_selenium_tools.regex_tools import get_file_extension, get_file_name_no_extension
 
 
 def find_no_ext(file_name, path):  # this will match a file_name
-    rg_pattern = r'[^.]+(?=\.)'
     result = []
     for root, dirs, files in os.walk(path):
-        for name in files:
-            name_no_ext = re.findall(rg_pattern, name)  # file name no extension
-            if len(name_no_ext) != 0:
-                if name_no_ext[0] == file_name:
-                    result.append(os.path.join(root, name))
+        for file in files:
+            if get_file_name_no_extension(file) == file_name:
+                result.append(os.path.join(root, file))
     return result  # возвращает список с путем к оригинальному файлу
+
+
+def action_with_image_files_in_directory(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if get_file_extension(file).lower() in ['jpg', 'jpeg']:
+                # you can add any function to work with file
+                print(f"{root}/{file}")
+
+
+if __name__ == '__main__':
+    # action_with_image_files_in_directory('/Volumes/big4photo-4/EDITED_JPEG_ARCHIV/Downloaded_from_fotoagency/Deleted_shoots')
+
+    assert type(find_no_ext('20230321PEV_9941-Edit', '/Volumes/big4photo-4/2023_main_archiv')) == list
+    assert '20230321PEV_9941-Edit' in find_no_ext('20230321PEV_9941-Edit', '/Volumes/big4photo-4/2023_main_archiv')[0]
