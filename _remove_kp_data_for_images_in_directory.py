addacbdd3c539505cfb26995fc6dc0bf254a0bb4
evgeniy <pavlinbl4@gmail.com>
2023-09-21 22:19:20 +0300

    function to clear kp info in images metadata

---
+++ _remove_kp_data_for_images_in_directory.py
@@ -0,0 +1,15 @@
+from kp_selenium_tools.tk_tools import select_folder_via_gui
+from shoot_history.exif_job import clear_image_metadata_from_kp_info
+from shoot_history.find_file_hdd import action_with_image_files_in_directory
+
+
+def main():
+    # select directory
+    initialdir = '/Volumes/big4photo-4/EDITED_JPEG_ARCHIV/Downloaded_from_fotoagency'
+    path_to_folder = select_folder_via_gui(initialdir)
+
+    action_with_image_files_in_directory(path_to_folder, clear_image_metadata_from_kp_info)
+
+
+if __name__ == '__main__':
+    main()
