7782460db721f6ab10da11eb48497f3a6b74e8c5
evgeniy <pavlinbl4@gmail.com>
2023-10-11 14:24:17 +0300

    refactoring

--- kp_selenium_tools/tk_tools.py
+++ kp_selenium_tools/tk_tools.py
@@ -1,13 +1,12 @@
 from tkinter import filedialog, messagebox, Tk
 
 
-
-def select_folder_via_gui(initialdir):
+def select_folder_via_gui(initial_dir):
     root = Tk()
     root.withdraw()
 
     choose_folder = filedialog.askdirectory(
-        initialdir=initialdir,
+        initialdir=initial_dir,
         title="Select your Source directory")
     if len(choose_folder) > 0:
         return choose_folder
