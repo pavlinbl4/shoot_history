5116deed342b5c827d97d2d3713ec749596cb3ee
evgeniy <pavlinbl4@gmail.com>
2023-10-11 09:28:38 +0300

    add filter to image files in "find_no_ext" function

--- shoot_history/find_file_hdd.py
+++ shoot_history/find_file_hdd.py
@@ -3,13 +3,11 @@ import os
 from kp_selenium_tools.regex_tools import get_file_extension, get_file_name_no_extension
 
 
-def find_no_ext(file_name, path):  # this will match a file_name
+def find_no_ext(file_name_no_extension: str, path: str):  # this will match a file_name
     result = []
     for root, dirs, files in os.walk(path):
-        for file in files:
-            # file_extension = get_file_extension(file)
-            # if file_extension in ('ORF', 'DNG', 'dng', 'NEF', 'nef', 'orf'):
-            if get_file_name_no_extension(file) == file_name:
+        for file in [elem for elem in files if  elem.split('.')[-1].lower() in ('dng',  'nef', 'orf', 'jpg', 'jpeg')]:
+            if get_file_name_no_extension(file) == file_name_no_extension:
                 result.append(os.path.join(root, file))
     return result  # возвращает список с путем к оригинальному файлу
 
@@ -29,3 +27,5 @@ if __name__ == '__main__':
 
     assert type(find_no_ext('20230321PEV_9941-Edit', '/Volumes/big4photo-4/2023_main_archiv')) == list
     assert '20230321PEV_9941-Edit' in find_no_ext('20230321PEV_9941-Edit', '/Volumes/big4photo-4/2023_main_archiv')[0]
+
+    assert len(find_no_ext('20231006EPAV2451', '/Users/evgeniy/Pictures/2023/20231006_Асфальто-бетонный завод')) == 1
