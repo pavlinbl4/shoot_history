398808cf558a23358da9594daab1ca9a5bfb3887
evgeniy <pavlinbl4@gmail.com>
2024-02-02 21:11:45 +0300

    remove auto update web driver

--- _shoot_history.py
+++ _shoot_history.py
@@ -1,6 +1,4 @@
-from auto_webdriver.update_driver import auto_update_webdriver
 from kp_selenium_tools.authorization import AuthorizationHandler
-
 from kp_selenium_tools.choose_input import clipboard_or_input
 from kp_selenium_tools.make_page_link import make_page_link
 from kp_selenium_tools.save_page_html import save_html_page, read_html
@@ -10,10 +8,7 @@ from kp_selenium_tools.write_xlsx import write_rename_voc
 from shoot_history.scrap_html import scrap_html
 
 
-
 def main():
-    auto_update_webdriver()
-
     path_to_file = '/Users/evgeniy/Documents/Kommersant/shoot_rename/shoot_story.xlsx'
 
     shoot_id = clipboard_or_input()
