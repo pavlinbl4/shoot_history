6157e35d9dfefd3b39ab5603b91d2150725a83a0
evgeniy <pavlinbl4@gmail.com>
2024-04-09 19:11:04 +0300

    set color tag to 'yellow'

--- mark_downloaded_from_kp_images_if_exist/main_check_files.py
+++ mark_downloaded_from_kp_images_if_exist/main_check_files.py
@@ -6,27 +6,31 @@ from loguru import logger
 from pathlib import Path
 import time
 from random import randint
+
 logger.disable('__main__')
 
 
 def example_function_to_work_with_files(path):
-
     # logger.info(path)
     photo_id = Path(path).stem[:-3]
     logger.info(photo_id)
-    time.sleep(randint(1,15))
+    time.sleep(randint(1, 15))
     # logger.info(find_image(photo_id))
-    if find_image(photo_id):
-        change_color_class(path, photo_id, color='Red')
+    if find_image(photo_id, driver):
+        change_color_class(path, photo_id, 'Red')
         logger.info(f'set red color to image {photo_id}')
     else:
+        change_color_class(path, photo_id, 'Yellow')
         clear_image_metadata_from_kp_info(path)
 
 
 def main():
     # check downloaded folder
+    # driver = AuthorizationHandler().authorize()
     action_with_image_files_in_directory('/Volumes/big4photo-4/selenium_downloads', example_function_to_work_with_files)
 
+
+driver = AuthorizationHandler().authorize()
 
 if __name__ == '__main__':
     main()
