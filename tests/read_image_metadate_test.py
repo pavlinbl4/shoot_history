f6e9c0a880e70610827a9512523bd1fc1d8a93bc
evgenii <pavlinbl4@gmail.com>
2024-04-09 12:08:54 +0300

    added new test

--- tests/read_image_metadate_test.py
+++ tests/read_image_metadate_test.py
@@ -2,12 +2,14 @@ from unittest import TestCase, main
 
 from shoot_history.exif_job import read_image_metadate
 
+TEST_JPEG = "/Users/evgenii/PycharmProjects/shoot_history/tests/test_files/20231214EPAV9435.jpg"
 
-class ReadImageMeyadata(TestCase):
+
+class ReadImageMetadata(TestCase):
     def test_read_image_metadate(self):
-        self.assertEqual(read_image_metadate('tests/test_files/20231214EPAV9435.jpg'), 'xxx')
+        self.assertIsInstance(read_image_metadate(TEST_JPEG), dict)
+        self.assertEqual(read_image_metadate(TEST_JPEG)['Composite:LensID'], 'OLYMPUS M.7-14mm F2.8')
 
 
 if __name__ == '__main__':
     main()
-    print(read_image_metadate('tests/test_files/20231214EPAV9435.jpg'))
