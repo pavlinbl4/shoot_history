870babbeab98739181b5a752329c83fb64495bff
evgeniy <pavlinbl4@gmail.com>
2024-04-09 19:18:23 +0300

    added new try/except

--- mark_downloaded_from_kp_images_if_exist/check_image_on_site.py
+++ mark_downloaded_from_kp_images_if_exist/check_image_on_site.py
@@ -7,17 +7,22 @@ from selenium.common import NoSuchElementException
 
 
 def find_image(image_id, driver):
-    photo_search_index_locator = ('xpath', "//input[@id='code']")
-    driver.find_element(*photo_search_index_locator).send_keys(image_id)
-
-    search_button_locator = ('xpath', "//input[@id='searchbtn']")
-    driver.find_element(*search_button_locator).click()
-
     try:
-        images_found_locator = ('xpath', "(//td[@class='note'])[18]")
-        driver.find_element(*images_found_locator)
-        print(f'image {image_id} is on site')
-        return True
+        photo_search_index_locator = ('xpath', "//input[@id='code']")
+        driver.find_element(*photo_search_index_locator).clear()
+        driver.find_element(*photo_search_index_locator).send_keys(image_id)
+
+        search_button_locator = ('xpath', "//input[@id='searchbtn']")
+        driver.find_element(*search_button_locator).click()
+
+        try:
+            images_found_locator = ('xpath', "(//td[@class='note'])[18]")
+            driver.find_element(*images_found_locator)
+            print(f'image {image_id} is on site')
+            return True
+        except NoSuchElementException as e:
+            print(f"image {image_id} not found")
+            return False
     except NoSuchElementException as e:
-        print(f"image {image_id} not found")
-        return False
+        print(e)
+
