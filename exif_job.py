7668c6506a492fd762b1e1475aedf70c760c5d77
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-12-14 18:22:24 +0300

    added image caption to write to change_color_class function

--- exif_job.py
+++ exif_job.py
@@ -1,19 +1,23 @@
 import exiftool
 from icecream import ic
 from kp_selenium_tools.regex_tools import modify_caption
+from loguru import logger
 
 
-def change_color_class(file, color, image_title=None, ):
+def change_color_class(path_to_file, photo_id, image_caption, color):
+    logger.info(f'{path_to_file = }')
+    logger.info(f'{photo_id = }')
+    logger.info(f'{image_caption = }')
     with exiftool.ExifToolHelper() as et:
         et.set_tags(
-            [file],
-            tags={"XMP:Title": image_title,
-                  "IPTC:ObjectName": image_title,
+            [path_to_file],
+            tags={"XMP:Title": photo_id,
+                  "IPTC:ObjectName": photo_id,
+                  'XMP:Description': image_caption,
                   'XMP:Label': color,
                   'XMP:Creator': 'Eugene Pavlenko'},
             params=["-P", "-overwrite_original"]
         )
-    print(f"metadata was changed to image {file}")
 
 
 def write_data_to_photo_iptc_tools(file):
@@ -51,7 +55,6 @@ def write_edited_tags(xmp_tags):
                     params=["-P", "-overwrite_original"])
 
 
-# get all image metadata from file
 def read_image_metadate(path_to_image_file):
     with exiftool.ExifToolHelper() as et:
         return et.get_metadata(path_to_image_file)[0]
@@ -68,22 +71,15 @@ def clear_image_metadata_from_kp_info(path_to_image_file):
     write_edited_tags(image_metadata)
 
 
-def read_image_tags(path_to_image_file):
-    with exiftool.ExifToolHelper() as et:
-        return et.get_tags(path_to_image_file, tags=['EXIF:Artist', 'XMP:Label'])[0]
-
-def read_image_run(path_to_image_file):
-    with exiftool.ExifToolHelper() as et:
-
-        return et.encoding, et.check_tag_names, et.last_status, et.common_args
-
-
 if __name__ == '__main__':
+
     # test_ol_image = '/Users/evgeniy/Pictures/test_images/20231006EPAV2225.ORF'
-    test_ol_image = "/Users/evgenii/PycharmProjects/shoot_history/tests/test_files/20231214EPAV9435.jpg"
-    # test_ol_image = ".test_files/20231214EPAV9435.jpg"
+    # test_ol_image = '/Users/evgeniy/PycharmProjects/shoot_history/tests/test_files/20231214EPAV9435.jpg'
+    # test_ol_image = '../shoot_history/tests/test_files/20231214EPAV9435.jpg'
     # write_data_to_photo_iptc_tools(test_ol_image)
-    change_color_class(test_ol_image, "Red", image_title=None)
+    change_color_class('/Users/evgeniy/Pictures/2024/12_December/20241211_Ветеринарный университет/20241211EPAV4896.ORF',
+                       'exiftool',
+                       "10.12.2024 Россия, Санкт-Петербург Участок Широтной магистрали скоростного движения (ШМСД) от Артиллерийского путепровода до Благодатной улицы",
+                       'Red')
     # ic(read_image_metadate(test_ol_image))
-    # print(read_image_metadate('tests/test_files/20231214EPAV9435.jpg'))
-    # ic(read_image_run(test_ol_image))
+
