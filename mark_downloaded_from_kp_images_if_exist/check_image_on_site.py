5758db00712f8301d0c6ea209446f86b258d9185
bigMac <bigMac@bigMac>
2025-07-15 15:59:45 +0300

    added Explicit Waits

--- mark_downloaded_from_kp_images_if_exist/check_image_on_site.py
+++ mark_downloaded_from_kp_images_if_exist/check_image_on_site.py
@@ -1,9 +1,6 @@
 from selenium.common import NoSuchElementException
-
-from kp_selenium_tools.authorization import AuthorizationHandler
-from kp_selenium_tools.selenium_tools import end_selenium
-
-from kp_selenium_tools.authorization import AuthorizationHandler
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 '''
 браузер должен быть запущен, чтоб не проходить авторизацию для каждого снимка заново
@@ -13,12 +10,21 @@ from kp_selenium_tools.authorization import AuthorizationHandler
 
 def grab_image_caption(driver):
     caption_found_locator = ('xpath', "(//td[@class='main'])[3]/p[@class='txt']")
+
+    WebDriverWait(driver, 5).until(
+        EC.visibility_of_element_located(caption_found_locator)
+    )
     return driver.find_element(*caption_found_locator).text
 
 
 def find_image(image_id, driver):
     try:
         photo_search_index_locator = ('xpath', "//input[@id='code']")
+
+        WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located(photo_search_index_locator)
+        )
+
         driver.find_element(*photo_search_index_locator).clear()
         driver.find_element(*photo_search_index_locator).send_keys(image_id)
 
@@ -27,6 +33,11 @@ def find_image(image_id, driver):
 
         try:
             images_found_locator = ('xpath', "(//td[@class='note'])[18]")
+
+            WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located(images_found_locator)
+            )
+
             driver.find_element(*images_found_locator)
             # print(f'image {image_id} is on site')
 
