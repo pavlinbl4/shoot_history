432743ed5b68f344806c7f1ce7c5e79d63ffff3e
bigMac <bigMac@bigMac>
2025-07-15 23:17:40 +0300

    отдельной функцией  получаю inner_id

--- utils/make_page_link.py
+++ utils/make_page_link.py
@@ -3,6 +3,9 @@ def make_page_link(shoot_id):
     return f"https://image.kommersant.ru/photo/wp/default.aspx?shootnum={shoot[1]}&sourcecode={shoot[0]}"
 
 
-def make_history_link(first_link):
-    inner_id = first_link.split('=')[1]
+def make_history_link(inner_id:str):
     return f'https://image.kommersant.ru/photo/archive/adm/ShootHistoryLog.aspx?id={inner_id}&insid=-{inner_id}'
+
+
+def get_inner_id(shoot_history_link:str):
+    return shoot_history_link.split('=')[1]
